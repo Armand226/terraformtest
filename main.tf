83eb1a9c32afffe8ce977a2dab0b9345c146ae9a
Armand226 <107824979+Armand226@users.noreply.github.com>
2023-08-25 00:54:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
 provider "azurerm" {
   features {}
-  tenant_id       = var.ARM_TENANT_ID
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  subscription_id = var.ARM_SUBSCRIPTION_ID
+  
 }
 resource "azurerm_resource_group" "example" {
   name     = "tesla"
