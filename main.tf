2cf7f775da2df8760750dd368434b41eb8c31a9b
Armand226 <107824979+Armand226@users.noreply.github.com>
2023-08-25 00:46:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "azurerm" {
   features {}
-  tenant_id       = "YOUR_TENANT_ID"
-  client_id       = "YOUR_CLIENT_ID"
-  client_secret   = "YOUR_CLIENT_SECRET"
-  subscription_id = "YOUR_SUBSCRIPTION_ID"
+  tenant_id       = var.ARM_TENANT_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  subscription_id = var.ARM_SUBSCRIPTION_ID
 }
 resource "azurerm_resource_group" "example" {
   name     = "my-resource-group"
-  location = "East US"
+  location = "South Central US"
 }
