53d173965649a4938bd9d0a11e0a733229b53b10
Armand226 <107824979+Armand226@users.noreply.github.com>
2023-08-25 14:32:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "terratest"
+    storage_account_name  = "piskaroro226"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
+
 provider "azurerm" {
   features {}
   
