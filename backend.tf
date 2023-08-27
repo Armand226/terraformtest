297811741ddd96f7bb2befd711b4cb95dae1bcbe
Armand226 <107824979+Armand226@users.noreply.github.com>
2023-08-26 22:01:03 -0500

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "terratest"
-    storage_account_name  = "piskaroro226"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
