541025188faeac3569253760873669020564664c
Armand226 <107824979+Armand226@users.noreply.github.com>
2023-08-26 21:01:03 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,15 +7,8 @@ variable "namespace_id" {
   description = "namespace_id"
   type        = string
 }
-variable "namespace_name" {
-  description = "namespace_name"
-  type        = string
-}
 
-variable "name" {
-  description = "resourcegroup_name"
-  type        = string
-}
+
 variable "name" {
   type = string
 }
