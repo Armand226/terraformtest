85d676ea981830ab94b4ce9df72c5db2e474eda7
Armand226 <107824979+Armand226@users.noreply.github.com>
2023-08-26 21:19:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,21 +2,22 @@ provider "azurerm" {
   features {}
   
 }
-resource "azurerm_servicebus_topic" "sbt" {
-  name                = var.name
-  resource_group_name = var.resource_group_name
-  namespace_name      = var.namespace_name
-
- 
-  default_message_ttl                     = var.default_message_ttl
-  enable_batched_operations               = var.enable_batched_operations
-  enable_express                          = var.enable_express
-  enable_partitioning                     = var.enable_partitioning
-  max_size_in_megabytes                   = var.max_size_in_megabytes
-  requires_duplicate_detection            = var.requires_duplicate_detection
-  support_ordering                        = var.support_ordering
-  duplicate_detection_history_time_window = var.duplicate_detection_history_time_window
+resource "azurerm_servicebus_topic" "sb" {
+  count                          = length (var.topic_names)
+  name                           = var.topic_names[count.index]
+  max_size_in_megabytes          = var.max_size_in_megabytes
+  namespace_id                   = var.namespace_id
+  enable_partitioning            = false  
 }
+
+resource "azurerm_servicebus_subscription" "sb" {
+  count                          = length (var.topic_names)
+  name                           = "${var.topic_names[count.index]}-subscription"
+  max_size_in_megabytes          = var.max_size_in_megabytes
+  topic_id                       = azurerm_servicebus_topic.sb[count.index].id
+  max_delivery_count             = 3
+}
+
 
 
 
