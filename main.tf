65f391ed3cf2f26286f7bd402c8f27369b063d35
Armand226 <107824979+Armand226@users.noreply.github.com>
2023-08-26 20:37:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_servicebus_topic" "example" {
   name                = var.topics[count.index]
   namespace_name      = azurerm_servicebus_namespace.example.name
   resource_group_name = "terratest"
-  namespace_id        = ""
+  namespace_id        = var.namespace_id
 }
 
 resource "azurerm_servicebus_subscription" "example" {
