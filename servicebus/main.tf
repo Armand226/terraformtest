2fa0efbc5095bb8427e6d7754da6fced1de4a2ce
Armand226 <107824979+Armand226@users.noreply.github.com>
2023-08-27 15:53:46 -0500

    Update main.tf

--- servicebus/main.tf
+++ servicebus/main.tf
@@ -2,11 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  resource_group_name     = var.resource_group_name
-  location                = var.location
-}
-
 resource "azurerm_servicebus_namespace" "sb" {
   name                = var.name
   location            = var.location
