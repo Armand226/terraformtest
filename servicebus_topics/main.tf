c6b4a7514a7d56319a15c8444d32d1158195e751
Armand226 <107824979+Armand226@users.noreply.github.com>
2023-08-26 22:00:06 -0500

    Create main.tf

---
+++ servicebus_topics/main.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+  features {}
+  
+}
+resource "azurerm_servicebus_topic" "sb" {
+  count                          = length (var.topic_names)
+  name                           = var.topic_names[count.index]
+  max_size_in_megabytes          = var.max_size_in_megabytes
+  namespace_id                   = var.namespace_id
+  enable_partitioning            = false  
+  default_message_ttl            = var.default_message_ttl
+}
+
+resource "azurerm_servicebus_subscription" "sb" {
+  count                          = length (var.topic_names)
+  name                           = "${var.subscription[count.index]}"
+  topic_id                       = azurerm_servicebus_topic.sb[count.index].id
+  max_delivery_count             = 3
+}
