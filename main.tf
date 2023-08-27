d3417c6eb25d39af51e3e0e1b23189175916655b
Armand226 <107824979+Armand226@users.noreply.github.com>
2023-08-26 20:28:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,22 @@ provider "azurerm" {
   
 }
 resource "azurerm_servicebus_namespace" "example" {
-  name                = "example-sbns"
+  name                = "sbns2254411"
   location            = "South central US"
   resource_group_name = "testing123"
   sku                 = "Standard"
+}
+resource "azurerm_servicebus_topic" "example" {
+  count               = length(var.topics)
+  name                = var.topics[count.index]
+  namespace_name      = azurerm_servicebus_namespace.example.name
+  resource_group_name = "terratest"
+}
+
+resource "azurerm_servicebus_subscription" "example" {
+  count               = length(var.topics)
+  name                = "${var.topics[count.index]}-subscription"
+  namespace_name      = azurerm_servicebus_namespace.example.name
+  topic_name          = azurerm_servicebus_topic.example[count.index].name
+  resource_group_name = "my-resource-group"
 }
