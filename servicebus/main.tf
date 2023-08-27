730787b54479aff68d121730c9587a248a3c6124
Armand226 <107824979+Armand226@users.noreply.github.com>
2023-08-27 15:48:39 -0500

    Update main.tf

--- servicebus/main.tf
+++ servicebus/main.tf
@@ -3,14 +3,14 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = var.resource_group
-  location = var.location
+  resource_group_name     = var.resource_group_name
+  location                = var.location
 }
 
-resource "azurerm_servicebus_namespace" "example" {
-  name                = "tfex-servicebus-namespace"
+resource "azurerm_servicebus_namespace" "sb" {
+  name                = var.name
   location            = var.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = var.resource_group_name
   sku                 = "Standard"
 
   tags = {
