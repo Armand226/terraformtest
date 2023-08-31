247b09f0bf3676a571df74586a462fe8c2912458
Armand226 <107824979+Armand226@users.noreply.github.com>
2023-08-30 20:12:07 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "South central US"
+}
+
+resource "azurerm_resource_group_template_deployment" "example" {
+  name                = "example-deployment"
+  resource_group_name = azurerm_resource_group.example.name
+  deployment_mode     = "Incremental"
+
+  template_content = storage-template.json"
+
+  parameters_content = jsonencode({
+    "storageAccountName" = {
+      "value" = "mystorageaccount"
+    },
+    "location" = {
+      "value" = "West Europe"
+    }
+  })
+}
