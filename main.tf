dc082eab9385eab4e5b9ef68682cf75d6aebf4f7
Armand226 <107824979+Armand226@users.noreply.github.com>
2023-08-30 19:44:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,10 @@ resource "azurerm_resource_group" "example" {
   location = "South Central US"
 }
 
-resource "azurerm_template_deployment" "example1" {
+resource "azurerm_template_deployment" "example" {
   name                = "example-deployment"
   resource_group_name = azurerm_resource_group.example.name
+  deployment_mode     = "Incremental"  # or "Complete"
 
   template_content = <<TEMPLATE
 {
