a7d4387a4141246c34b2e9a1b6a4cc4cf3019b5a
Armand226 <107824979+Armand226@users.noreply.github.com>
2023-09-04 03:06:45 -0500

    Update main.tf

--- flow/main.tf
+++ flow/main.tf
@@ -3,9 +3,9 @@ provider "azurerm" {
 }
 
 resource "azurerm_app_service_plan" "example" {
-  name                = "azure-functions-test-service-plan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  name                = "azure-functions-test-serv-plan"
+  location            = "South Central US"
+  resource_group_name = "terratest"
   kind                = "elastic"
 
 
