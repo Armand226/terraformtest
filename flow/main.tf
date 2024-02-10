360cfbad2521e56d3f9375e4104f4675120d98b6
Armand226 <107824979+Armand226@users.noreply.github.com>
2024-02-09 20:25:57 -0600

    Update main.tf

--- flow/main.tf
+++ flow/main.tf
@@ -2,7 +2,8 @@ resource "azurerm_linux_web_app" "example" {
   name                = var.app_service_name
   location            = "East US"  # Change it to your desired location
   resource_group_name = var.resource_group_name  # Replace with your existing resource group name
-  app_service_plan_id = "/subscriptions/subscription-id/resourceGroups/existing-resource-group-name/providers/Microsoft.Web/serverfarms/${var.app_service_plan_name}"
+  app_service_plan_id = "/subscriptions/subscription-id/resourceGroups/existing-resource-group-name/providers/Microsoft.Web/serverfarms/${var.app_service_plan_id}"
+
   site_config {
     linux_fx_version = "NODE|18"
   }
