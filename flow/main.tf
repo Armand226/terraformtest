ce5a8c650d1d1080986ae3f91576f09bac262e0b
Armand226 <107824979+Armand226@users.noreply.github.com>
2024-02-09 20:21:40 -0600

    Update main.tf

--- flow/main.tf
+++ flow/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service" "example" {
+resource "azurerm_linux_web_app" "example" {
   name                = var.app_service_name
   location            = "East US"  # Change it to your desired location
   resource_group_name = var.resource_group_name  # Replace with your existing resource group name
