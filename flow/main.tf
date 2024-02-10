0b10d37cb9ec1085dd688a5ab8adee71839617e3
Armand226 <107824979+Armand226@users.noreply.github.com>
2024-02-09 20:24:12 -0600

    Update main.tf

--- flow/main.tf
+++ flow/main.tf
@@ -3,6 +3,12 @@ resource "azurerm_linux_web_app" "example" {
   location            = "East US"  # Change it to your desired location
   resource_group_name = var.resource_group_name  # Replace with your existing resource group name
   app_service_plan_id = "/subscriptions/subscription-id/resourceGroups/existing-resource-group-name/providers/Microsoft.Web/serverfarms/${var.app_service_plan_name}"
+  site_config {
+    linux_fx_version = "NODE|18"
+  }
 
-  # Additional configurations...
+  app_settings = {
+    "WEBSITE_NODE_DEFAULT_VERSION" = "18.14.0"  # Specify Node.js 18 version
+  
+  }
 }
