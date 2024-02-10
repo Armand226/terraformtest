1941afb4f68758713e8b8f0f7324df7522d8001d
Armand226 <107824979+Armand226@users.noreply.github.com>
2024-02-09 20:18:30 -0600

    Update main.tf

--- flow/main.tf
+++ flow/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_app_service" "example" {
   name                = var.app_service_name
   location            = "East US"  # Change it to your desired location
-  resource_group_name = "existing-resource-group-name"  # Replace with your existing resource group name
+  resource_group_name = var.resource_group_name  # Replace with your existing resource group name
   app_service_plan_id = "/subscriptions/subscription-id/resourceGroups/existing-resource-group-name/providers/Microsoft.Web/serverfarms/${var.app_service_plan_name}"
 
   # Additional configurations...
