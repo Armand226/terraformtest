cf15e62a9f01ae6e07aadda55d21a94ffbc36ac9
Armand226 <107824979+Armand226@users.noreply.github.com>
2024-02-09 20:15:57 -0600

    Create variables.tf

---
+++ flow/variables.tf
@@ -0,0 +1,13 @@
+variable "resource_group_name" {
+  type    = string
+  description = "The name of the Azure App Service"
+}
+variable "app_service_name" {
+  type    = string
+  description = "The name of the Azure App Service"
+}
+
+variable "app_service_plan_name" {
+  type    = string
+  description = "The name of the Azure App Service Plan"
+}
