a700a0abc79c14e90c7180e94f8e17e59eab3ea2
Armand226 <107824979+Armand226@users.noreply.github.com>
2024-02-09 20:26:12 -0600

    Update variables.tf

--- flow/variables.tf
+++ flow/variables.tf
@@ -2,7 +2,7 @@ variable "resource_group_name" {
   type    = string
   description = "The name of the Azure App Service"
 }
-variable "app_service_name" {
+variable "app_service_plan_id" {
   type    = string
   description = "The name of the Azure App Service"
 }
