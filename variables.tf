77961808f3b8cadf91e132beb17ab20b7d6d6dbb
Armand226 <107824979+Armand226@users.noreply.github.com>
2025-03-19 19:38:55 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,25 @@
 variable "public_key" {
   description = "MongoDB Atlas Public API Key"
   type        = string
+default = "lhjgxkoj"
 }
 
 variable "private_key" {
   description = "MongoDB Atlas Private API Key"
   type        = string
-  sensitive   = true
+  #sensitive   = true
+default = "d357ed2d-9143-4483-8c7c-627083d25612"
 }
 
 variable "org_id" {
   description = "MongoDB Atlas Organization ID"
   type        = string
+default = "67db4a8785bbe2297b42d298"
 }
 
 variable "db_password" {
   description = "Database User Password"
   type        = string
-  sensitive   = true
+  #sensitive   = true
+default = "admin123"
 }
