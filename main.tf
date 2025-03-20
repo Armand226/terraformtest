cefd33f1b54cb41d1c34131f9c8d3ef8464d9822
Armand226 <107824979+Armand226@users.noreply.github.com>
2025-03-19 21:10:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "mongodbatlas_cluster" "my_cluster" {
   provider_name              = "AZURE"  # Azure as the provider
   provider_instance_size_name = "M0"    # Free-tier cluster
   backing_provider_name      = "AZURE"
-  provider_region_name       = "US_EAST_2" # Correct argument for Azure region
+  provider_region_name       = "US_EAST" # Correct argument for Azure region
 }
 
 
