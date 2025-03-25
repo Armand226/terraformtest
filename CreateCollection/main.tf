7cab6e2e7938870d8267789b6190743a257235d4
Armand226 <107824979+Armand226@users.noreply.github.com>
2025-03-25 15:01:06 -0500

    Update main.tf

--- CreateCollection/main.tf
+++ CreateCollection/main.tf
@@ -45,12 +45,11 @@ resource "mongodbatlas_database_user" "user" {
 resource "null_resource" "create_collection" {
   provisioner "local-exec" {
     command = <<EOT
-    sleep 60
       mongosh "${data.mongodbatlas_cluster.cluster.connection_strings[0].standard_srv}" \
         --username ${var.mongodb_username} \
         --password ${var.mongodb_password} \
         --eval "use ${var.mongodb_database_name};
-        db.createCollection("${var.mongodb_collection_name}")"
+        db.createCollection("${var.mongodb_collection_name}");"
     EOT
   }
 depends_on = [mongodbatlas_project_ip_access_list.ip_whitelist]
